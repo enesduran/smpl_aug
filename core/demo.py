ecf2ac4ad821d70221c4bb78b7539488239af587
Enes Duran <eduran2@login2.is.localnet>
2024-08-04 17:29:16 +0200

    start setting pipeline

--- core/demo.py
+++ core/demo.py
@@ -21,6 +21,7 @@ import smpl_aug
 import argparse
 import numpy as np
 import torch.nn as nn
+from pytorch3d import transforms
 from simkinect.add_noise_smpl_no_discrete import mesh_2_kinectpcd
 
 ### compatibility with python 2.7
@@ -37,32 +38,98 @@ class SMPL_WRAPPER(nn.Module):
     def __init__(self,
                  model_folder,
                  body_model_type,
-                ext='npz',
-                gender='neutral',
-                num_betas=10,
-                camera_config="",
-                use_face_contour=False):
+                 clothing_option,
+                 ext='npz',
+                 gender='neutral',
+                 num_betas=10,
+                 camera_config="",
+                 use_face_contour=False,
+                 use_layer=False):
         
         super(SMPL_WRAPPER, self).__init__()
 
+        self.use_layer = use_layer  
         self.body_model_type = body_model_type
 
-        self.model = smpl_aug.create(model_path=model_folder,
-                        model_type=body_model_type,
-                        gender=gender, 
-                        use_face_contour=use_face_contour,
-                        num_betas=num_betas,
-                        ext=ext,
-                        use_pca=False,
-                        clothing_option='clothed')
+        if use_layer:
+            self.model = smpl_aug.build_layer(model_path=model_folder,
+                                                    model_type=body_model_type, 
+                                                    clothing_option=clothing_option)
+        else:
+            self.model = smpl_aug.create(model_path=model_folder,
+                            model_type=body_model_type,
+                            gender=gender, 
+                            use_face_contour=use_face_contour,
+                            num_betas=num_betas,
+                            ext=ext,
+                            use_pca=False,
+                            clothing_option=clothing_option)
 
 
         self.camera_config = camera_config
 
 
-    def forward(self, motion_path):
+    def forward(self, betas, expression, global_orient, transl, reye_pose, leye_pose, 
+                jaw_pose, left_hand_pose, right_hand_pose, body_pose, **kwargs_dict):
+        """
+        Args:
+            betas: torch.Tensor, shape [N, 10]
+            expression: torch.Tensor, shape [N, 10]
+            global_orient: torch.Tensor, shape [N, 3]
+            transl: torch.Tensor, shape [N, 3]
+            reye_pose: torch.Tensor, shape [N, 3]
+            leye_pose: torch.Tensor, shape [N, 3]
+            jaw_pose: torch.Tensor, shape [N, 3]
+            left_hand_pose: torch.Tensor, shape [N, 45]
+            right_hand_pose: torch.Tensor, shape [N, 45]
+            body_pose: torch.Tensor, shape [N, 72]
+            kwargs_dict: dict, additional arguments to be passed to the model"""
 
+       
+        if self.use_layer:
+            
+            T = body_pose.shape[0]
+            
+            # Layers accept rotmat only. This is a legacy of smplx library.
+            body_pose_rotmat = transforms.axis_angle_to_matrix(body_pose.reshape(T, -1, 3))
+            global_orient_rotmat = transforms.axis_angle_to_matrix(global_orient.reshape(T, -1, 3))
+
+            output = self.model_layer(betas=betas, 
+                        expression=expression,
+                        global_orient=global_orient_rotmat,
+                        transl=transl,
+                        reye_pose=reye_pose,
+                        leye_pose=leye_pose,
+                        jaw_pose=jaw_pose,
+                        left_hand_pose=left_hand_pose,
+                        right_hand_pose=right_hand_pose,
+                        body_pose=body_pose_rotmat,
+                        **kwargs_dict)
+            
+        else:
+            output = self.model(betas=betas, 
+                        expression=expression,
+                        global_orient=global_orient,
+                        transl=transl,
+                        reye_pose=reye_pose,
+                        leye_pose=leye_pose,
+                        jaw_pose=jaw_pose,
+                        left_hand_pose=left_hand_pose,
+                        right_hand_pose=right_hand_pose,
+                        body_pose=body_pose,
+                        **kwargs_dict)
+           
+        vertices = output.vertices.detach().cpu().numpy().squeeze()
+ 
+        trimesh.Trimesh(vertices[0], self.model.faces).export('test.obj')
+        
+
+        mesh_2_kinectpcd(vertices, self.model.faces, camera_config_file=self.camera_config)
+
+    
+    def augment(self, motion_path):
         motion_dict = np.load(motion_path)
+
         motion_T = motion_dict["poses"].shape[0]
         motion_T = min(motion_T, 120)
 
@@ -98,25 +165,11 @@ class SMPL_WRAPPER(nn.Module):
         global_orient = torch.tensor(motion_dict["poses"][:, :3], dtype=torch.float32)[:motion_T]
         betas = torch.tensor(motion_dict["betas"][None, :10], dtype=torch.float32).repeat(motion_T, 1)
         expression = torch.zeros_like(betas)
+         
+        self.forward(betas, expression, global_orient, transl, reye_pose, leye_pose,
+                    jaw_pose, left_hand_pose, right_hand_pose, body_pose, **kwargs_dict)
 
-        output = self.model(betas=betas, 
-                    expression=expression,
-                    global_orient=global_orient,
-                    transl=transl,
-                    reye_pose=reye_pose,
-                    leye_pose=leye_pose,
-                    jaw_pose=jaw_pose,
-                    left_hand_pose=left_hand_pose,
-                    right_hand_pose=right_hand_pose,
-                    body_pose=body_pose,
-                    **kwargs_dict)
-
-        vertices = output.vertices.detach().cpu().numpy().squeeze()
- 
-        trimesh.Trimesh(vertices[0], self.model.faces).export('test.obj')
         
-
-        mesh_2_kinectpcd(vertices, self.model.faces, camera_config_file=self.camera_config)
     
 
 if __name__ == '__main__':
@@ -124,6 +177,8 @@ if __name__ == '__main__':
 
     parser.add_argument('--body-model-type', choices=['smplx', 'smplh', 'smpl'], type=str,
                         help='Body Model to be used.')
+    parser.add_argument('--clothing-option', choices=['clothed', 'minimal'], type=str,
+                        default='clothed', help='Flag for garmenting minimal body.')
     parser.add_argument('--model-folder', required=True, type=str,
                         help='The path to the model folder')
     parser.add_argument('--motion-path', required=True, type=str,
@@ -146,6 +201,7 @@ if __name__ == '__main__':
                                gender=args.gender,
                                num_betas=10,
                                camera_config=args.camera_config,
-                               use_face_contour=args.use_face_contour) 
+                               use_face_contour=args.use_face_contour, 
+                               clothing_option=args.clothing_option) 
     
-    wrapper_obj(args.motion_path)
+    wrapper_obj.augment(args.motion_path)
