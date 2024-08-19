6e76812c39adadb9fd7a07f3f88358965b0f2370
Enes Duran <eduran2@login2.is.localnet>
2024-08-19 03:00:58 +0200

    fix mesh point cloud misalgnment issue, move methods to utils

--- core/simkinect/add_noise_smpl_no_discrete.py
+++ core/simkinect/add_noise_smpl_no_discrete.py
@@ -5,215 +5,15 @@ import torch
 import numpy as np 
 import open3d as o3d
 from loguru import logger
-from simkinect.camera_utils import vertex_from_depth
+from simkinect.camera_utils import filterDisp, add_gaussian_shifts
 
 from pytorch3d.structures import Meshes
 from pytorch3d.implicitron.tools import point_cloud_utils
 from pytorch3d.renderer import RasterizationSettings, MeshRasterizer, PerspectiveCameras, look_at_view_transform 
+
+
+
  
-
-def add_gaussian_shifts(depth, std=1/2.0):
-
-    if len(depth.shape) == 2:
-        rows, cols = depth.shape 
-        bs = 1 
-    else:
-        bs, rows, cols = depth.shape 
-
-    gaussian_shifts = np.random.normal(0, std, size=(bs, rows, cols, 2))
-    gaussian_shifts = gaussian_shifts.astype(np.float32)
- 
-    # creating evenly spaced coordinates  
-    xx = np.linspace(0, cols-1, cols)
-    yy = np.linspace(0, rows-1, rows)
-
-    # get xpixels and ypixels 
-    xp, yp = np.meshgrid(xx, yy)
-
-    xp = xp.astype(np.float32)
-    yp = yp.astype(np.float32)
-
-    xp = np.repeat(xp[None, ...], bs, axis=0) 
-    yp = np.repeat(yp[None, ...], bs, axis=0) 
-
-    xp_interp = np.minimum(np.maximum(xp + gaussian_shifts[..., 0], 0.0), cols)
-    yp_interp = np.minimum(np.maximum(yp + gaussian_shifts[..., 1], 0.0), rows)
-
-    depth_interp = np.array([cv2.remap(depth[i], xp_interp[i], yp_interp[i], cv2.INTER_LINEAR) for i in range(bs)])
-
-    return depth_interp
-    
-
-def filterDisp(disp, dot_pattern_, invalid_disp_, size_filt_):
-
-    xx = np.linspace(0, size_filt_-1, size_filt_)
-    yy = np.linspace(0, size_filt_-1, size_filt_)
-
-    xf, yf = np.meshgrid(xx, yy)
-
-    xf = xf - int(size_filt_ / 2.0)
-    yf = yf - int(size_filt_ / 2.0)
-
-    sqr_radius = (xf**2 + yf**2)
-    vals = sqr_radius * 1.2**2 
-
-    vals[vals==0] = 1 
-    weights_ = 1 /vals  
-
-    fill_weights = 1 / ( 1 + sqr_radius)
-    fill_weights[sqr_radius > size_filt_] = -1.0 
-
-    disp_rows, disp_cols = disp.shape 
-    dot_pattern_rows, dot_pattern_cols = dot_pattern_.shape
-
-    lim_rows = disp_rows - size_filt_
-    lim_cols = disp_cols - size_filt_
-
-    center = int(size_filt_ / 2.0)
-
-    window_inlier_distance_ = 0.1
-
-    out_disp = np.ones_like(disp) * invalid_disp_
-
-    interpolation_map = np.zeros_like(disp)
-
-    for r in range(0, lim_rows):
-
-        for c in range(0, lim_cols):
-            r_dot, c_dot = r, c
-            if (dot_pattern_rows - size_filt_ < r):
-                r_dot = r % (dot_pattern_rows - size_filt_)
-
-            if (dot_pattern_cols - size_filt_ < c):
-                c_dot = c % (dot_pattern_cols - size_filt_)
-
-            if dot_pattern_[r_dot+center, c_dot+center] > 0:
-                                
-                # c and r are the top left corner 
-                window  = disp[r:r+size_filt_, c:c+size_filt_] 
-                dot_win = dot_pattern_[r_dot:r_dot+size_filt_, c_dot:c_dot+size_filt_] 
-  
-                valid_dots = dot_win[window < invalid_disp_]
-
-                n_valids = np.sum(valid_dots) / 255.0 
-                n_thresh = np.sum(dot_win) / 255.0 
-
-                if n_valids > n_thresh / 1.2: 
-
-                    mean = np.mean(window[window < invalid_disp_])
-
-                    diffs = np.abs(window - mean)
-                    diffs = np.multiply(diffs, weights_)
-
-                    cur_valid_dots = np.multiply(np.where(window<invalid_disp_, dot_win, 0), 
-                                                 np.where(diffs < window_inlier_distance_, 1, 0))
-
-                    n_valids = np.sum(cur_valid_dots) / 255.0
-
-                    if n_valids > n_thresh / 1.2: 
-                    
-                        accu = window[center, center] 
-
-                        assert(accu < invalid_disp_)
-
-                        #out_disp[r+center, c + center] = round((accu)*8.0) / 8.0 # discretization
-                        out_disp[r+center, c + center] = accu ########################
-
-                        interpolation_window = interpolation_map[r:r+size_filt_, c:c+size_filt_]
-                        disp_data_window     = out_disp[r:r+size_filt_, c:c+size_filt_]
-
-                        substitutes = np.where(interpolation_window < fill_weights, 1, 0)
-                        interpolation_window[substitutes==1] = fill_weights[substitutes ==1 ]
-
-                        disp_data_window[substitutes==1] = out_disp[r+center, c+center]
-
-    return out_disp
-
-def filterDisp_short(disp, dot_pattern_, invalid_disp_, size_filt_):
-
-    xx = np.linspace(0, size_filt_-1, size_filt_)
-    yy = np.linspace(0, size_filt_-1, size_filt_)
-
-    xf, yf = np.meshgrid(xx, yy)
-
-    xf = xf - int(size_filt_ / 2.0)
-    yf = yf - int(size_filt_ / 2.0)
-
-    sqr_radius = (xf**2 + yf**2)
-    vals = sqr_radius * 1.2**2 
-
-    vals[vals==0] = 1 
-    weights_ = 1 /vals  
-
-    fill_weights = 1 / ( 1 + sqr_radius)
-    fill_weights[sqr_radius > size_filt_] = -1.0 
-
-    disp_rows, disp_cols = disp.shape 
-    dot_pattern_rows, dot_pattern_cols = dot_pattern_.shape
-
-    lim_rows = disp_rows - size_filt_
-    lim_cols = disp_cols - size_filt_
-
-    center = int(size_filt_ / 2.0)
-
-    window_inlier_distance_ = 0.1
-
-    out_disp = np.ones_like(disp) * invalid_disp_
-
-    interpolation_map = np.zeros_like(disp)
-
-    for r in range(0, lim_rows):
-
-        for c in range(0, lim_cols):
-            r_dot, c_dot = r, c
-            if (dot_pattern_rows - size_filt_ < r):
-                r_dot = r % (dot_pattern_rows - size_filt_)
-
-            if (dot_pattern_cols - size_filt_ < c):
-                c_dot = c % (dot_pattern_cols - size_filt_)
-
-            if dot_pattern_[r_dot+center, c_dot+center] > 0:
-                                
-                # c and r are the top left corner 
-                window  = disp[r:r+size_filt_, c:c+size_filt_] 
-                dot_win = dot_pattern_[r_dot:r_dot+size_filt_, c_dot:c_dot+size_filt_] 
-  
-                valid_dots = dot_win[window < invalid_disp_]
-
-                n_valids = np.sum(valid_dots) / 255.0 
-                n_thresh = np.sum(dot_win) / 255.0 
-
-                if n_valids > n_thresh / 1.2: 
-
-                    mean = np.mean(window[window < invalid_disp_])
-
-                    diffs = np.abs(window - mean)
-                    diffs = np.multiply(diffs, weights_)
-
-                    cur_valid_dots = np.multiply(np.where(window<invalid_disp_, dot_win, 0), 
-                                                 np.where(diffs < window_inlier_distance_, 1, 0))
-
-                    n_valids = np.sum(cur_valid_dots) / 255.0
-
-                    if n_valids > n_thresh / 1.2: 
-                    
-                        accu = window[center, center] 
-
-                        assert(accu < invalid_disp_)
-
-                        #out_disp[r+center, c + center] = round((accu)*8.0) / 8.0 # discretization
-                        out_disp[r+center, c + center] = accu ########################
-
-                        interpolation_window = interpolation_map[r:r+size_filt_, c:c+size_filt_]
-
-                        substitutes = np.where(interpolation_window < fill_weights, 1, 0)
-                        interpolation_window[substitutes==1] = fill_weights[substitutes ==1]
-
-                        disp_data_window     = out_disp[r:r+size_filt_, c:c+size_filt_]
-                        disp_data_window[substitutes==1] = out_disp[r+center, c+center]                 
-
-    return out_disp
-
 def filterDisp_batch(disp, dot_pattern_, invalid_disp_, size_filt_):
     """
     Filter the disparity map using the dot pattern and the disparity map
@@ -300,10 +100,7 @@ def filterDisp_batch(disp, dot_pattern_, invalid_disp_, size_filt_):
  
                     diffs = np.abs(window_ind - mean)
                     diffs = np.multiply(diffs, weights_)
-                    
-                    # cur_valid_dots = np.multiply(np.where(window<invalid_disp_, dot_win, 0), 
-                    #                              np.where(diffs < window_inlier_distance_, 1, 0))
-                    
+                     
                     cur_valid_dots = np.multiply(np.where(window_ind<invalid_disp_, dot_win[indices0], 0), 
                                                  np.where(diffs < window_inlier_distance_, 1, 0))
                     
@@ -312,8 +109,7 @@ def filterDisp_batch(disp, dot_pattern_, invalid_disp_, size_filt_):
                     channelwise_flag1 = n_valids > (n_thresh[indices0] / 1.2) 
 
                     if channelwise_flag1.any(): 
-
-                        # import ipdb; ipdb.set_trace()
+ 
                         indices1 = channelwise_flag1.nonzero()[0]
     
                         # keep track of the channels to update the disp map
@@ -354,30 +150,33 @@ def capture_mesh_depth(meshes, camera, image_size):
 
     return depth
 
-def recover_pcd_from_depth(depth_noised_tensor, camera):
+def recover_pcd_from_depth(depth_noised_tensor, cameras, single_pc_flag=False):
 
     bs, h, w = depth_noised_tensor.shape
         
-
     depth_noised_tensor = depth_noised_tensor.reshape(bs, 1, h, w)
     # dummy rgb values
     rgb = torch.ones((bs, 3, h, w)).to(depth_noised_tensor) * 0.5
 
-    projected_pcd_noised_list = [point_cloud_utils.get_rgbd_point_cloud(camera=camera[i], 
+    if single_pc_flag:
+        projected_pcd_noised_list = point_cloud_utils.get_rgbd_point_cloud(camera=cameras, 
+                                                                        image_rgb=rgb, 
+                                                                        depth_map=depth_noised_tensor)
+    else:
+        projected_pcd_noised_list = [point_cloud_utils.get_rgbd_point_cloud(camera=cameras[i], 
                                                                   image_rgb=rgb[i][None], 
                                                                   depth_map=depth_noised_tensor[i][None]) for i in range(bs)]
     
-    # vertex_from_depth(depth=depth_noised_tensor,
-    #                   fl=camera[0].focal_length,
-    #                   pp=[h/2, w/2],
-    #                   depth_range=10,
-    #                   png_scale_factor=1) 
-
     return projected_pcd_noised_list
 
 
-def mesh2pcd(vertices, faces, camera_config_dict, cameras_batch, kinect_dot_pattern, 
+def mesh2pcd(vertices, 
+             faces, 
+             camera_config_dict, 
+             cameras_batch, 
+             kinect_dot_pattern, 
              upsample_mesh=False, 
+             single_pc_flag=False,
              device=torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")):
     """ 
     Takes in the vertices and faces of the mesh and the camera configuration and
@@ -403,10 +202,8 @@ def mesh2pcd(vertices, faces, camera_config_dict, cameras_batch, kinect_dot_patt
     baseline_m    = 0.075   # baseline in m 0.075
     INVALID_DISP_THRESHOLD = 99999999.9
     filter_size_window = camera_config_dict['conv_filter_size_window']
+    fx = camera_config_dict['focal_length_x']
 
-    # world units
-    # fx, fy = camera_config_dict['focal_length_x'], camera_config_dict['focal_length_y']
- 
      
     if upsample_mesh:
         # Apply the subdivision algorithm to the mesh
@@ -429,21 +226,16 @@ def mesh2pcd(vertices, faces, camera_config_dict, cameras_batch, kinect_dot_patt
     depth_gt = (depth_gt_unscaled- depth_gt_unscaled.min()) / (depth_gt_unscaled.max() - depth_gt_unscaled.min() + 1e-10)
 
     depth_interp = add_gaussian_shifts(depth_gt) # ok
-    # depth_f = fx * baseline_m / (depth_interp + 1e-10)
-    depth_f = 100 * baseline_m / (depth_interp + 1e-10)
+    depth_f = fx * baseline_m / (depth_interp + 1e-10)
     
     out_disp = np.array([filterDisp(depth_f[i], kinect_dot_pattern, INVALID_DISP_THRESHOLD, filter_size_window) for i in range(num_cameras)])
-    # out_disp_short = filterDisp_short(depth_f[0], kinect_dot_pattern, INVALID_DISP_THRESHOLD, filter_size_window)[None]
-    # out_disp_ = filterDisp_batch(depth_f, kinect_dot_pattern, INVALID_DISP_THRESHOLD, filter_size_window)
-    # print(f"DIFF {abs(out_disp - out_disp_).sum()}")
- 
-    # depth = fx * baseline_m / out_disp
-    depth = 100 * baseline_m / out_disp
-    
+    # out_disp = filterDisp_batch(depth_f, kinect_dot_pattern, INVALID_DISP_THRESHOLD, filter_size_window)
+
+    depth = fx * baseline_m / out_disp
+        
     # The depth here needs to converted to centimeters so scale factor is introduced 
     # though often this can be tuned from [100, 200] to get the desired banding / quantisation effects 
     # noisy_depth = (35130/np.round((35130/np.round(depth*scale_factor)) + np.random.normal(size=depth.shape)*(1.0/6.0) + 0.5))/scale_factor # discretization
-    
     noisy_depth = (35130/np.round(depth*scale_factor)) 
     noisy_depth += np.random.normal(size=depth.shape)*(1.0/6.0)
     noisy_depth = np.round(noisy_depth + 0.5)
@@ -452,9 +244,8 @@ def mesh2pcd(vertices, faces, camera_config_dict, cameras_batch, kinect_dot_patt
 
     depth[out_disp == INVALID_DISP_THRESHOLD] = 0 
     
-    # Recover point cloud from noised depth image 
-    projected_pcd_noised = recover_pcd_from_depth(torch.from_numpy(noisy_depth).to(device), cameras_batch)
+    projected_pcd_noised = recover_pcd_from_depth(torch.from_numpy(depth_gt_unscaled).to(device), cameras_batch, single_pc_flag=single_pc_flag)
  
-    return depth_gt, depth, noisy_depth, projected_pcd_noised
+    return depth_gt_unscaled, depth_gt, depth, noisy_depth, projected_pcd_noised
 
     
--- core/simkinect/camera_utils.py
+++ core/simkinect/camera_utils.py
@@ -25,12 +25,13 @@ def vertex_from_depth(depth,
                           to png image 
 
     '''
-        
+    
     fl_x, fl_y = fl 
     pp_x, pp_y = pp 
     min_depth, max_depth = depth_range
 
     rows, cols = depth.shape
+    # bs, rows, cols = depth.shape
 
     # creating evenly spaced coordinates  
     xx = np.linspace(0, cols-1, cols)
@@ -51,51 +52,18 @@ def vertex_from_depth(depth,
     vx = (xp - pp_x) * depth_m / fl_x 
     vy = (yp - pp_y) * depth_m / fl_y 
     vz = depth_m 
-
+ 
     vertices = np.transpose(np.stack([vx, vy, vz]), (1, 2, 0))
 
-    return vertices, depth_m 
-
-def normal_from_vertex(vertices):
-
-    '''
-        This function takes a point cloud and 
-        returns normal vectors at each point
-        
-        @params
-        vertices: input vertices that are used 
-                  to compute the normals via 
-                  cross product
-    '''
-
-    Vr = np.zeros_like(vertices)
-    Vu = np.zeros_like(vertices)
-
-    rows, cols, _ = vertices.shape
-
-    Vu[0: rows-1, :, :] = vertices[1: rows, :, :]
-    Vr[:, 0: cols-1, :] = vertices[:, 1:cols,  :]
-
-    a = Vr - vertices
-    b = Vu - vertices 
-
-    axb = np.cross(a, b)
-
-    mag = np.linalg.norm(axb, axis=-1)
-    mag[mag < 1e-10] = 1 
-
-    axb[...,0] = axb[...,0] / mag 
-    axb[...,1] = axb[...,1] / mag 
-    axb[...,2] = axb[...,2] / mag 
-
-    return axb 
+    return vertices # , depth_m 
 
 def add_gaussian_shifts(depth, std=1/2.0):
 
-    rows, cols = depth.shape 
-    gaussian_shifts = np.random.normal(0, std, size=(rows, cols, 2))
-    gaussian_shifts = gaussian_shifts.astype(np.float32)
+    bs, rows, cols = depth.shape 
 
+    gaussian_shifts = np.random.normal(0, std, size=(bs, rows, cols, 2))
+    gaussian_shifts = gaussian_shifts.astype(np.float32)
+ 
     # creating evenly spaced coordinates  
     xx = np.linspace(0, cols-1, cols)
     yy = np.linspace(0, rows-1, rows)
@@ -106,19 +74,17 @@ def add_gaussian_shifts(depth, std=1/2.0):
     xp = xp.astype(np.float32)
     yp = yp.astype(np.float32)
 
-    xp_interp = np.minimum(np.maximum(xp + gaussian_shifts[:, :, 0], 0.0), cols)
-    yp_interp = np.minimum(np.maximum(yp + gaussian_shifts[:, :, 1], 0.0), rows)
+    xp = np.repeat(xp[None, ...], bs, axis=0) 
+    yp = np.repeat(yp[None, ...], bs, axis=0) 
 
-    depth_interp = cv2.remap(depth, xp_interp, yp_interp, cv2.INTER_LINEAR)
+    xp_interp = np.minimum(np.maximum(xp + gaussian_shifts[..., 0], 0.0), cols)
+    yp_interp = np.minimum(np.maximum(yp + gaussian_shifts[..., 1], 0.0), rows)
+
+    depth_interp = np.array([cv2.remap(depth[i], xp_interp[i], yp_interp[i], cv2.INTER_LINEAR) for i in range(bs)])
 
     return depth_interp
-    
 
-def filterDisp(disp, dot_pattern_):
-
-    invalid_disp_ = 99999999.9
-
-    size_filt_ = 9
+def filterDisp(disp, dot_pattern_, invalid_disp_, size_filt_):
 
     xx = np.linspace(0, size_filt_-1, size_filt_)
     yy = np.linspace(0, size_filt_-1, size_filt_)
@@ -135,13 +101,13 @@ def filterDisp(disp, dot_pattern_):
     weights_ = 1 /vals  
 
     fill_weights = 1 / ( 1 + sqr_radius)
-    fill_weights[sqr_radius > 9] = -1.0 
+    fill_weights[sqr_radius > size_filt_] = -1.0 
 
     disp_rows, disp_cols = disp.shape 
     dot_pattern_rows, dot_pattern_cols = dot_pattern_.shape
 
-    lim_rows = np.minimum(disp_rows - size_filt_, dot_pattern_rows - size_filt_)
-    lim_cols = np.minimum(disp_cols - size_filt_, dot_pattern_cols - size_filt_)
+    lim_rows = disp_rows - size_filt_
+    lim_cols = disp_cols - size_filt_
 
     center = int(size_filt_ / 2.0)
 
@@ -154,12 +120,18 @@ def filterDisp(disp, dot_pattern_):
     for r in range(0, lim_rows):
 
         for c in range(0, lim_cols):
+            r_dot, c_dot = r, c
+            if (dot_pattern_rows - size_filt_ < r):
+                r_dot = r % (dot_pattern_rows - size_filt_)
 
-            if dot_pattern_[r+center, c+center] > 0:
+            if (dot_pattern_cols - size_filt_ < c):
+                c_dot = c % (dot_pattern_cols - size_filt_)
+
+            if dot_pattern_[r_dot+center, c_dot+center] > 0:
                                 
                 # c and r are the top left corner 
                 window  = disp[r:r+size_filt_, c:c+size_filt_] 
-                dot_win = dot_pattern_[r:r+size_filt_, c:c+size_filt_] 
+                dot_win = dot_pattern_[r_dot:r_dot+size_filt_, c_dot:c_dot+size_filt_] 
   
                 valid_dots = dot_win[window < invalid_disp_]
 
@@ -184,7 +156,8 @@ def filterDisp(disp, dot_pattern_):
 
                         assert(accu < invalid_disp_)
 
-                        out_disp[r+center, c + center] = round((accu)*8.0) / 8.0
+                        #out_disp[r+center, c + center] = round((accu)*8.0) / 8.0 # discretization
+                        out_disp[r+center, c + center] = accu ########################
 
                         interpolation_window = interpolation_map[r:r+size_filt_, c:c+size_filt_]
                         disp_data_window     = out_disp[r:r+size_filt_, c:c+size_filt_]
@@ -195,56 +168,3 @@ def filterDisp(disp, dot_pattern_):
                         disp_data_window[substitutes==1] = out_disp[r+center, c+center]
 
     return out_disp
-
-if __name__ == "__main__":
-
-
-    dot_pattern_ = cv2.imread("./data/kinect-pattern_3x3.png", 0)
-
-    count = 181
-
-    scale_factor = 100 
-    focal_length = 480.0
-    baseline_m = 0.075
-
-    while True:
-
-        depth_uint16 = cv2.imread("depth/{}.png".format(count), cv2.IMREAD_UNCHANGED)
-        h, w = depth_uint16.shape 
-
-        # Our depth images were scaled by 5000 to store in png format so dividing to get 
-        # depth in meters 
-        depth = depth_uint16.astype('float') / 5000.0
-
-        depth_interp = add_gaussian_shifts(depth)
-
-        disp_= focal_length * baseline_m / (depth_interp + 1e-10)
-
-        depth_f = np.round(disp_ * 8.0)/8.0
-
-        out_disp = filterDisp(depth_f, dot_pattern_)
-
-        depth = focal_length * baseline_m / out_disp
-        depth[out_disp == 99999999.9] = 0 
-        
-
-        # The depth here needs to converted to cms so scale factor is introduced 
-        # though often this can be tuned from [100, 200] to get the desired banding / quantisation effects 
-        noisy_depth = (35130/np.round((35130/np.round(depth*scale_factor)) + np.random.normal(size=(h, w))*(1.0/6.0) + 0.5))/scale_factor 
-
-        noisy_depth = noisy_depth * 5000.0 
-        noisy_depth = noisy_depth.astype('uint16')
-
-        # Displaying side by side the orignal depth map and the noisy depth map with barron noise cvpr 2013 model
-        cv2.namedWindow('Adding Kinect Noise', cv2.WINDOW_AUTOSIZE)
-        # cv2.imshow('Adding Kinect Noise', np.hstack((depth_uint16, noisy_depth)))
-        cv2.imshow('Adding Kinect Noise', np.hstack((depth_uint16, noisy_depth)))
-        key = cv2.waitKey(1)
-
-        # Press esc or 'q' to close the image window
-        if key & 0xFF == ord('q') or key == 27:
-            cv2.destroyAllWindows()
-            break
-
-        print(count)
-        count = (count + 1)% 500
