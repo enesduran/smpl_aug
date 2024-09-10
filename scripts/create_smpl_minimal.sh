d4999d54ead1b1b23cae56626803d0bba03a0181
Enes Duran <eduran2@login2.is.localnet>
2024-09-10 23:27:00 +0200

    add scripts

---
+++ scripts/create_smpl_minimal.sh
@@ -0,0 +1,6 @@
+python core/demo.py \
+    --model-folder body_models \
+    --body-model-type smpl \
+    --motion-path motion_data/sample_motion_data_smpl.npz \
+    --camera-config camera_configs/kinect.json \
+    --clothing-option minimal 
